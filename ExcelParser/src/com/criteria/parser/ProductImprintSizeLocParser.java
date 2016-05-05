ca47bfdfd0094a7b0fb56f14c23cce02416c68a3
Amey <amey.more@a4technology.com>
2016-05-05 17:19:32 +0530

    Class missed

---
+++ ExcelParser/src/com/criteria/parser/ProductImprintSizeLocParser.java
@@ -0,0 +1,17 @@
+package com.criteria.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.a4.product.beans.ImprintSizeLocation;
+
+public class ProductImprintSizeLocParser {
+	public synchronized List<ImprintSizeLocation> getimpsizeLocCriteria(String sizevalue,String locaTionValue,boolean flag){
+		
+		// to process this there is some problem with ASI Product on ui for this criteria not able to work on this now
+		
+		return null;
+				
+		
+	}
+}
